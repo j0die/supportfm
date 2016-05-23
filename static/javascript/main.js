bf22937cc90cfc294f281a8d4d9b6b476525c9d5
Blaine O'Neill <blaine.d.oneill@gmail.com>
2016-05-23 01:10:38 -0700

    insert commas into numbers

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function($){
 	$('.donate-button').hover(
 	function(){$('body').toggleClass('rainbow');}
 	);
+  	
 	
 	var $timeline_block = $('.cd-timeline-block');
 
@@ -13,7 +14,7 @@ jQuery(document).ready(function($){
 		}
 	});
 
-	//on scolling, show/animate timeline blocks when enter the viewport
+	//on scrolling, show/animate timeline blocks when enter the viewport
 	$(window).on('scroll', function(){
 		$timeline_block.each(function(){
 			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
