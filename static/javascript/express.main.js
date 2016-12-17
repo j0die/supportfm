ff5bef2b010b6cdc422d106e5a065e4542f184ca
puppycodes <woof@puppy.codes>
2016-12-17 03:58:32 -0800

    add project folders

--- static/javascript/express.main.js
+++
@@ -1,9 +0,0 @@
-	$(document).ready(function(){
-    $('.modal').modal();
-    
-    if ($.cookie('modal_shown') == null) {
-        $.cookie('modal_shown', 'yes', { expires: 7, path: '/' });
-        $('#about-modal').modal('open');
-    }
-    
-  });
